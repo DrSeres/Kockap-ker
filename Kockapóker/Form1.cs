4dbf919ad3b8fa2386404f2f1233f165a71e165e
SERES <seresszabolcs01@gmail.com>
2022-10-04 13:20:24 +0200

    Véletlen számok

--- Kockapóker/Form1.cs
+++ Kockapóker/Form1.cs
@@ -7,18 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Kockapóker.sajatOsztalyok;
 
 namespace Kockapóker
 {
     public partial class Form1 : Form
     {
+        Leosztas l = new Leosztas();
         public Form1()
         {
             InitializeComponent();
+            pbElsoJatekos1.Image = Properties.Resources.kek1;
+            pbElsoJatekos2.Image = Properties.Resources.kek2;
+            pbElsoJatekos3.Image = Properties.Resources.kek3;
+            pbElsoJatekos4.Image = Properties.Resources.kek4;
+            pbElsoJatekos5.Image = Properties.Resources.kek5;
         }
 
         private void btnKilepes_Click(object sender, EventArgs e)
         {
+            MessageBox.Show(l.ToString());
             Application.Exit();
         }
     }
---
+++ Kockapóker/sajatOsztalyok/Leosztas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kockapóker.sajatOsztalyok
+{
+    class Leosztas
+    {
+        List<int> kockak = new List<int>();
+
+        public Leosztas()
+        {
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+
+            for (int i = 0; i < 5; i++)
+            {
+                kockak.Add(rnd.Next(1, 6));
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder tmp = new StringBuilder("");
+            foreach (var kocka in kockak)
+            {
+                tmp.Append($"{kocka} ");
+            }
+            return tmp.ToString();
+        }
+    }
+}
