74d9dc810500edefd259522b2eedf076915a83d6
SERES <seresszabolcs01@gmail.com>
2022-10-04 12:53:41 +0200

    Kockák

--- Kockapóker/Form1.Designer.cs
+++ Kockapóker/Form1.Designer.cs
@@ -29,13 +29,209 @@ namespace Kockapóker
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnKilepes = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pbElsoJatekos1 = new System.Windows.Forms.PictureBox();
+            this.pbElsoJatekos2 = new System.Windows.Forms.PictureBox();
+            this.pbElsoJatekos3 = new System.Windows.Forms.PictureBox();
+            this.pbElsoJatekos4 = new System.Windows.Forms.PictureBox();
+            this.pbElsoJatekos5 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pbMasodikJatekos5 = new System.Windows.Forms.PictureBox();
+            this.pbMasodikJatekos4 = new System.Windows.Forms.PictureBox();
+            this.pbMasodikJatekos3 = new System.Windows.Forms.PictureBox();
+            this.pbMasodikJatekos2 = new System.Windows.Forms.PictureBox();
+            this.pbMasodikJatekos1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbElsoJatekos1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbElsoJatekos2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbElsoJatekos3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbElsoJatekos4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbElsoJatekos5)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMasodikJatekos5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMasodikJatekos4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMasodikJatekos3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMasodikJatekos2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMasodikJatekos1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnKilepes
+            // 
+            this.btnKilepes.Location = new System.Drawing.Point(947, 12);
+            this.btnKilepes.Name = "btnKilepes";
+            this.btnKilepes.Size = new System.Drawing.Size(110, 37);
+            this.btnKilepes.TabIndex = 0;
+            this.btnKilepes.Text = "Kilepes";
+            this.btnKilepes.UseVisualStyleBackColor = true;
+            this.btnKilepes.Click += new System.EventHandler(this.btnKilepes_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pbElsoJatekos5);
+            this.groupBox1.Controls.Add(this.pbElsoJatekos4);
+            this.groupBox1.Controls.Add(this.pbElsoJatekos3);
+            this.groupBox1.Controls.Add(this.pbElsoJatekos2);
+            this.groupBox1.Controls.Add(this.pbElsoJatekos1);
+            this.groupBox1.Location = new System.Drawing.Point(18, 367);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(453, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Első játékos";
+            // 
+            // pbElsoJatekos1
+            // 
+            this.pbElsoJatekos1.Location = new System.Drawing.Point(6, 24);
+            this.pbElsoJatekos1.Name = "pbElsoJatekos1";
+            this.pbElsoJatekos1.Size = new System.Drawing.Size(70, 70);
+            this.pbElsoJatekos1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbElsoJatekos1.TabIndex = 0;
+            this.pbElsoJatekos1.TabStop = false;
+            // 
+            // pbElsoJatekos2
+            // 
+            this.pbElsoJatekos2.Location = new System.Drawing.Point(90, 24);
+            this.pbElsoJatekos2.Name = "pbElsoJatekos2";
+            this.pbElsoJatekos2.Size = new System.Drawing.Size(70, 70);
+            this.pbElsoJatekos2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbElsoJatekos2.TabIndex = 0;
+            this.pbElsoJatekos2.TabStop = false;
+            // 
+            // pbElsoJatekos3
+            // 
+            this.pbElsoJatekos3.Location = new System.Drawing.Point(176, 24);
+            this.pbElsoJatekos3.Name = "pbElsoJatekos3";
+            this.pbElsoJatekos3.Size = new System.Drawing.Size(70, 70);
+            this.pbElsoJatekos3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbElsoJatekos3.TabIndex = 0;
+            this.pbElsoJatekos3.TabStop = false;
+            // 
+            // pbElsoJatekos4
+            // 
+            this.pbElsoJatekos4.Location = new System.Drawing.Point(261, 24);
+            this.pbElsoJatekos4.Name = "pbElsoJatekos4";
+            this.pbElsoJatekos4.Size = new System.Drawing.Size(70, 70);
+            this.pbElsoJatekos4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbElsoJatekos4.TabIndex = 0;
+            this.pbElsoJatekos4.TabStop = false;
+            // 
+            // pbElsoJatekos5
+            // 
+            this.pbElsoJatekos5.Location = new System.Drawing.Point(348, 24);
+            this.pbElsoJatekos5.Name = "pbElsoJatekos5";
+            this.pbElsoJatekos5.Size = new System.Drawing.Size(70, 70);
+            this.pbElsoJatekos5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbElsoJatekos5.TabIndex = 0;
+            this.pbElsoJatekos5.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pbMasodikJatekos5);
+            this.groupBox2.Controls.Add(this.pbMasodikJatekos4);
+            this.groupBox2.Controls.Add(this.pbMasodikJatekos3);
+            this.groupBox2.Controls.Add(this.pbMasodikJatekos2);
+            this.groupBox2.Controls.Add(this.pbMasodikJatekos1);
+            this.groupBox2.Location = new System.Drawing.Point(18, 489);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(453, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Második játékos";
+            // 
+            // pbMasodikJatekos5
+            // 
+            this.pbMasodikJatekos5.Location = new System.Drawing.Point(348, 24);
+            this.pbMasodikJatekos5.Name = "pbMasodikJatekos5";
+            this.pbMasodikJatekos5.Size = new System.Drawing.Size(70, 70);
+            this.pbMasodikJatekos5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMasodikJatekos5.TabIndex = 0;
+            this.pbMasodikJatekos5.TabStop = false;
+            // 
+            // pbMasodikJatekos4
+            // 
+            this.pbMasodikJatekos4.Location = new System.Drawing.Point(261, 24);
+            this.pbMasodikJatekos4.Name = "pbMasodikJatekos4";
+            this.pbMasodikJatekos4.Size = new System.Drawing.Size(70, 70);
+            this.pbMasodikJatekos4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMasodikJatekos4.TabIndex = 0;
+            this.pbMasodikJatekos4.TabStop = false;
+            // 
+            // pbMasodikJatekos3
+            // 
+            this.pbMasodikJatekos3.Location = new System.Drawing.Point(176, 24);
+            this.pbMasodikJatekos3.Name = "pbMasodikJatekos3";
+            this.pbMasodikJatekos3.Size = new System.Drawing.Size(70, 70);
+            this.pbMasodikJatekos3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMasodikJatekos3.TabIndex = 0;
+            this.pbMasodikJatekos3.TabStop = false;
+            // 
+            // pbMasodikJatekos2
+            // 
+            this.pbMasodikJatekos2.Location = new System.Drawing.Point(90, 24);
+            this.pbMasodikJatekos2.Name = "pbMasodikJatekos2";
+            this.pbMasodikJatekos2.Size = new System.Drawing.Size(70, 70);
+            this.pbMasodikJatekos2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMasodikJatekos2.TabIndex = 0;
+            this.pbMasodikJatekos2.TabStop = false;
+            // 
+            // pbMasodikJatekos1
+            // 
+            this.pbMasodikJatekos1.Location = new System.Drawing.Point(6, 24);
+            this.pbMasodikJatekos1.Name = "pbMasodikJatekos1";
+            this.pbMasodikJatekos1.Size = new System.Drawing.Size(70, 70);
+            this.pbMasodikJatekos1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMasodikJatekos1.TabIndex = 0;
+            this.pbMasodikJatekos1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::Kockapóker.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1086, 601);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnKilepes);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kockapóker";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbElsoJatekos1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbElsoJatekos2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbElsoJatekos3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbElsoJatekos4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbElsoJatekos5)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMasodikJatekos5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMasodikJatekos4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMasodikJatekos3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMasodikJatekos2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMasodikJatekos1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnKilepes;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pbElsoJatekos5;
+        private System.Windows.Forms.PictureBox pbElsoJatekos4;
+        private System.Windows.Forms.PictureBox pbElsoJatekos3;
+        private System.Windows.Forms.PictureBox pbElsoJatekos2;
+        private System.Windows.Forms.PictureBox pbElsoJatekos1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pbMasodikJatekos5;
+        private System.Windows.Forms.PictureBox pbMasodikJatekos4;
+        private System.Windows.Forms.PictureBox pbMasodikJatekos3;
+        private System.Windows.Forms.PictureBox pbMasodikJatekos2;
+        private System.Windows.Forms.PictureBox pbMasodikJatekos1;
     }
 }
 
--- Kockapóker/Properties/Resources.Designer.cs
+++ Kockapóker/Properties/Resources.Designer.cs
@@ -8,9 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace Kockapóker.Properties
-{
+namespace Kockapóker.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -18,52 +19,174 @@ namespace Kockapóker.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Kockapóker.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap background {
+            get {
+                object obj = ResourceManager.GetObject("background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap kek1 {
+            get {
+                object obj = ResourceManager.GetObject("kek1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap kek2 {
+            get {
+                object obj = ResourceManager.GetObject("kek2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap kek3 {
+            get {
+                object obj = ResourceManager.GetObject("kek3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap kek4 {
+            get {
+                object obj = ResourceManager.GetObject("kek4", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap kek5 {
+            get {
+                object obj = ResourceManager.GetObject("kek5", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap kek6 {
+            get {
+                object obj = ResourceManager.GetObject("kek6", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap zold1 {
+            get {
+                object obj = ResourceManager.GetObject("zold1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap zold2 {
+            get {
+                object obj = ResourceManager.GetObject("zold2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap zold3 {
+            get {
+                object obj = ResourceManager.GetObject("zold3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap zold4 {
+            get {
+                object obj = ResourceManager.GetObject("zold4", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap zold5 {
+            get {
+                object obj = ResourceManager.GetObject("zold5", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap zold6 {
+            get {
+                object obj = ResourceManager.GetObject("zold6", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
